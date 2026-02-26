c5652da1bee06cdc55f1c965caeffc883c38aa0e
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-02-26 11:43:45 -0300

    Add files via upload

---
+++ script banco de dados 2/condicionaisOrInAnd26-02-26.sql
@@ -0,0 +1,7 @@
+select * from tabela_de_produtos where sabor = 'Manga' and tamanho = '3 ml';
+
+select * from tabela_de_produtos where sabor = 'Manga' and not (tamanho = '470 ml');
+select * from tabela_de_produtos where sabor = 'Manga' or not (tamanho = '470 ml');
+select * from tabela_de_produtos where sabor in ('Manga', 'Laranja');
+
+select * from tabela_de_clientes where cidade in ('Rio de Janeiro', 'São Paulo') and idade >= 20;
